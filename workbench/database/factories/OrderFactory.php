2582074cfb1764d21b408f8e34e32c5d2c3a96d6
a-sabagh <a.sabagh72@gmail.com>
2025-08-26 14:54:41 +0330

    workbench order factory

---
+++ workbench/database/factories/OrderFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Workbench\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Workbench\App\Models\Order;
+
+class OrderFactory extends Factory
+{
+    protected $model = Order::class;
+
+    public function definition(): array
+    {
+        return [];
+    }
+}
