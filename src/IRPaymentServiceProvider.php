8d0e9d23dbf58f9b735a63ed3c12ddf6270015b0
a-sabagh <a.sabagh72@gmail.com>
2025-08-26 15:09:31 +0330

    merge package configuration

--- src/IRPaymentServiceProvider.php
+++ src/IRPaymentServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class IRPaymentServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/irpayment.php', 'irpayment'
+        );
+    }
+
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
