11fb17c74bedd1bc401f947e72d9ead3f9c0985d
a-sabagh <a.sabagh72@gmail.com>
2025-08-29 16:08:09 +0330

    redefine web routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 use IRPayment\Http\Controllers\PaymentController;
+use IRPayment\Http\Controllers\ZarinpalPaymentController;
 
-Route::get('payment/verify/{payment}', [PaymentController::class, 'verify'])
-    ->name('payment.verify')
+Route::get('payment/zarinpal/verify', [ZarinpalPaymentController::class, 'verify'])
+    ->name('payment.zarinpal.verify');
+
+Route::get('payment/details/{payment}', [PaymentController::class, 'details'])
+    ->name('payment.details')
     ->middleware([SubstituteBindings::class]);
