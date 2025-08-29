b74cc7476482af89956df2f5f3d8286f984cf5dc
a-sabagh <a.sabagh72@gmail.com>
2025-08-29 16:12:32 +0330

    remove useless namespace

--- src/Models/Payment.php
+++ src/Models/Payment.php
@@ -3,7 +3,6 @@
 namespace IRPayment\Models;
 
 use Illuminate\Contracts\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
