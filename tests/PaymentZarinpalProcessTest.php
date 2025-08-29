d840b75c9accd005c6d7e0e5430959075463bf70
a-sabagh <a.sabagh72@gmail.com>
2025-08-29 19:06:27 +0330

    test zarinpal payment process behaviour

---
+++ tests/PaymentZarinpalProcessTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace IRPayment\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use IRPayment\Facades\IRPayment;
+use IRPayment\Models\Payment;
+use Workbench\App\Models\Order;
+
+use function Orchestra\Testbench\workbench_path;
+
+class PaymentZarinpalProcessTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $requestResponse = file_get_contents(__DIR__.'/fake/zarinpal/request.json');
+
+        Http::fake([
+            'https://api.zarinpal.com/pg/v4/payment/request.json' => Http::response($requestResponse, 200),
+        ]);
+
+    }
+
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadMigrationsFrom(
+            workbench_path('database/migrations')
+        );
+    }
+
+    public function test_zarinpal_process_payment_with_payment_object(): void
+    {
+        $this->app->config->set('irpayment.drivers.zarinpal', [
+            'merchant_id' => '1234',
+            'currency' => 'IRR',
+        ]);
+
+        $order = Order::factory()->create();
+
+        $payment = Payment::factory()->for(
+            $order, 'paymentable'
+        )->create();
+
+        IRPayment::driver('zarinpal')->process($payment);
+
+        $this->assertSame($payment->authority_key, 'A0000000000000000000000000000wwOGYpd');
+
+        Http::assertSent(function (Request $request) use ($payment) {
+            return
+                $request['currency'] == 'IRR' &&
+                $request['amount'] == $payment->amount &&
+                $request['description'] == $payment->description &&
+                $request['merchant_id'] == '1234';
+        });
+    }
+}
