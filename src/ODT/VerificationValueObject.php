16b009fadb2d3066cc51dbe149c7cb88c440f327
a-sabagh <a.sabagh72@gmail.com>
2025-08-29 16:15:20 +0330

    verification value object

---
+++ src/ODT/VerificationValueObject.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace IRPayment\ODT;
+
+class VerificationValueObject
+{
+    public function __construct(
+        public int $code,
+        public int $referenceId,
+        public string $message,
+        public string $cardHash,
+        public string $cardMask
+    ) {}
+
+    public function isSuccess(): bool
+    {
+        return in_array($this->code, [100, 101]);
+    }
+
+    public function isFailed(): bool
+    {
+        return ! $this->isSuccess();
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'code' => $this->code,
+            'reference_id' => $this->referenceId,
+            'message' => $this->message,
+            'card_hash' => $this->cardHash,
+            'card_mask' => $this->cardMask,
+        ];
+    }
+}
