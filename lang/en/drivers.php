fc34814c9ae98dbb590118d6cced57db24a3235d
a-sabagh <a.sabagh72@gmail.com>
2025-08-31 10:53:37 +0330

    set driver strings

--- lang/en/drivers.php
+++ lang/en/drivers.php
@@ -1,3 +1,24 @@
 <?php
 
-return [];
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Payment Drivers (English)
+    |--------------------------------------------------------------------------
+    | You can also configure the driver in your configuration files, 
+    | However, this is not recommended because it never changes based on your application locale.
+    | The array keys below refer to the corresponding payment drivers.
+    */
+
+    'card_transfer' => [
+        'title' => 'Card-to-Card Transfer',
+        'description' => 'Pay offline by transferring money directly from your card to the merchant’s card.',
+    ],
+
+    'zarinpal' => [
+        'title' => 'Zarinpal Payment Gateway',
+        'description' => 'Pay securely online via the Zarinpal payment gateway.',
+    ],
+
+];
