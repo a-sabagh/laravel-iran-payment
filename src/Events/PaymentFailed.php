82fc17e8a1cd03213feceac9f41e7aa727a15b77
a-sabagh <a.sabagh72@gmail.com>
2025-09-01 14:19:22 +0330

    payment failed event

---
+++ src/Events/PaymentFailed.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IRPayment\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use IRPayment\DTO\VerificationValueObject;
+use IRPayment\Models\Payment;
+
+class PaymentFailed
+{
+    use Dispatchable;
+
+    public function __construct(
+        public Payment $payment,
+        public VerificationValueObject $verification
+    ) {}
+}
