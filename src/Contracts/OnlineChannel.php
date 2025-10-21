86f553822a0143358b486697007b39e3ef0ac268
a-sabagh <a.sabagh72@gmail.com>
2025-10-21 16:44:49 +0330

    temporary remove verify interface

--- src/Contracts/OnlineChannel.php
+++ src/Contracts/OnlineChannel.php
@@ -9,6 +9,4 @@ interface OnlineChannel
     public function CallbackUrl(): string;
 
     public function process(Payment $payment);
-
-    public function verify(int $amount, string $authorityKey);
 }
