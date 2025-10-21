4670838890e47b1be390faf54382c16450a130f0
a-sabagh <a.sabagh72@gmail.com>
2025-10-21 16:45:47 +0330

    payping verify controller action

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 use IRPayment\Http\Controllers\PaymentController;
+use IRPayment\Http\Controllers\PaypingPaymentController;
 use IRPayment\Http\Controllers\ZarinpalPaymentController;
 
 Route::get('payment/zarinpal/verify', [ZarinpalPaymentController::class, 'verify'])
     ->name('payment.zarinpal.verify');
+
+Route::get('payment/payping/verify', [PaypingPaymentController::class, 'verify'])
+    ->name('payment.payping.verify');
 
 Route::get('payment/details/{payment}', [PaymentController::class, 'details'])
     ->name('payment.details')
---
+++ src/Http/Controllers/PaypingPaymentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace IRPayment\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use IRPayment\Facades\IRPayment;
+use IRPayment\Repositories\PaymentRepository;
+
+class PaypingPaymentController extends Controller
+{
+    public function verify(Request $request, PaymentRepository $paymentRepo)
+    {
+        $encodedData = $request->string('data');
+
+        $request->merge([
+            'data' => json_decode($encodedData, true),
+        ]);
+
+        $validator = Validator::make($request->all(), [
+            'data' => ['required', 'array'],
+            'data.paymentCode' => ['required', 'numeric'],
+            'data.clientRefId' => ['required', 'numeric', 'exists:payments.id'],
+            'status' => ['required', 'boolean'],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->handleInvalidRequest($validator);
+        }
+
+        $authorityKey = $paymentCode = $request->integer('data.paymentCode');
+        $payment = $paymentRepo->findByAuthorityKey($authorityKey);
+
+        if (! $request->status) {
+            return $this->handleCanceledPayment($payment);
+        }
+
+        $amount = $payment->amount;
+        $paymentRefId = $request->integer('data.paymentRefId');
+
+        $verification = IRPayment::driver('payping')
+            ->verify($amount, $paymentRefId, $paymentCode);
+
+        if ($verification->isFailed()) {
+            return $this->handleFailedPayment($payment, $verification);
+        }
+
+        return $this->handleVerifiedPayment($payment, $verification);
+    }
+}
